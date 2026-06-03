d844184352a3a1d798e6d1f87c9db14a81ba482d
wihl21br <wiinhula.2023@gmail.com>
2026-06-03 15:05:26 -0300

    aula_3/6/2026

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+const server = Bun.serve({
+    port:3000,
+    routes:{
+        "/api-debugger": Bun.file("puplic/api-debugger.html"),
+        "/test": {
+            GET: ()=> Response.json({ time: Date.now()}),
+            PUT: ()=> Response.json({ time: Date.now()}),
+            POST: ()=> Response.json({ time: Date.now()}),
+            DELETE: ()=> Response.json({ time: Date.now()})
+        }
+    },
+    fetch(){
+        return new Response('not found', { status:404 })
+    }
+})
+
+console.log(`⚡ http://localhost:${server.port}`)
